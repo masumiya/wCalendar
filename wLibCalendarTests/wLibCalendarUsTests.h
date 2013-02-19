0dc735467034df526b7bbd91a8127020f556263a
Yuichi Masumiya <aspec360m@i.softbank.jp>
2013-02-19 21:22:10 +0900

    アメリカ合衆国の公的祝日判定を実装

---
+++ wLibCalendarTests/wLibCalendarUsTests.h
@@ -0,0 +1,19 @@
+//
+//  wLibCalendarUsTests.h
+//  wLibCalendar
+//
+//  Created by masumiya on 2013/02/19.
+//  Copyright (c) 2013年 masumiya. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "wLibCalendarUs.h"
+
+@interface wLibCalendarUsTests : SenTestCase
+{
+@private
+    wLibCalendarUs* cal;
+    
+}
+
+@end
